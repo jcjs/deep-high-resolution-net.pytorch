a5d12a6089a7e65d9791dc3313d9c52b081365c6
jcjs <juancarlos.jsalom@gmail.com>
2019-06-04 13:40:31 +0000

    Updates

--- tools/run_on_custom_patches.py
+++ tools/run_on_custom_patches.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 
 import argparse
 import os
-import pprint
+# import pprint
 
 import torch
 import torch.nn.parallel
@@ -37,8 +37,10 @@ from mmdet.apis import inference_detector
 import time
 import json
 import math
-import glob
-import numpy as np
+# import glob
+# import numpy as np
+# from PIL import Image
+
 
 # "keypoints": [
 #             "nose","left_eye","right_eye","left_ear","right_ear",
@@ -46,6 +48,10 @@ import numpy as np
 #             "left_wrist","right_wrist","left_hip","right_hip",
 #             "left_knee","right_knee","left_ankle","right_ankle"
 #         ],
+
+
+def remove_trailing_slash(str):
+    return str[:-1] if str[-1] is '/' else str
 
 
 def read_bboxes(jspath, input_path, expand=1.0):
@@ -68,17 +74,19 @@ def read_bboxes(jspath, input_path, expand=1.0):
         img = mmcv.imread('{}/{}'.format(input_path, img_fname))
         for r in results_dict[img_fname]:
             lt, rb = r['bbox']['lt'], r['bbox']['rb']  # left top, right bottom corners
-            final_bbox_coords.append((lt, rb))
 
             lt = [int(dim*(1-expand/100)) for dim in lt]  # apply expansion
             rb = [int(dim*(1+expand/100)) for dim in rb]
+
+            final_bbox_coords.append((lt, rb))
 
             bbox = img[lt[1]:rb[1], lt[0]:rb[0]]  # crop
             bboxes.append(bbox)
             img_fnames.append(img_fname)
             # mmcv.imshow(bbox)  # debug
 
-    return dict(img_fnames=img_fnames, image_size=image_size, bboxes=bboxes, final_bbox_coords=final_bbox_coords)
+    return dict(img_fnames=img_fnames, image_size=image_size,
+                bboxes=bboxes, final_bbox_coords=final_bbox_coords)
 
 
 def calc_avg_aspect_ratio(bboxes):
@@ -90,67 +98,114 @@ def calc_avg_aspect_ratio(bboxes):
 
 
 def calc_angle(p1, p2):
+    '''
+    :param p1:
+    :param p2:
+    :return:
+    '''
     if (p2[0]-p1[0]) == 0:
         return math.pi/2
     else:
         return math.atan((p2[1]-p1[1])/(p2[0]-p1[0]))
 
 
-def project_point_along_line(init_point, theta, distance):
-    new_x = init_point[0] + distance * math.cos(theta)
-    new_y = init_point[1] - distance * math.sin(theta)
+def project_point_along_line(init_point, theta, magnitude, p_value):
+    '''
+    :param init_point:
+    :param theta:
+    :param distance:
+    :return:
+    '''
+    new_x = init_point[0] + p_value*magnitude * math.cos(theta)
+    new_y = init_point[1] - p_value*magnitude * abs(math.sin(theta))
     return int(new_x), int(new_y)
 
 
-def draw_final_points(img, img_fname, coords_list, scores, final_bbox_coords, output_path):
-    '''
-    :param img:
-    :param coords_list:
-    :param scores:
-    :return:
+def get_valeo_pd_ann(coords_list, final_bbox_coords):
     '''
 
-    new_path = '{}/{}_poses.jpg'.format(output_path, os.path.splitext(os.path.basename(img_fname))[0])
+    :param coords_list:
+    :param final_bbox_coords:
+    :return:
+    '''
+    bbox_rightbottom = final_bbox_coords[1]
+
+    left_ankle = coords_list[15]
+    left_ankle_to_bbox = abs(left_ankle[1] - bbox_rightbottom[1])
+    right_ankle = coords_list[16]
+    right_ankle_to_bbox = abs(right_ankle[1] - bbox_rightbottom[1])
+    ears_middle_point = (int((coords_list[3][0] + coords_list[4][0]) / 2),
+                   int((coords_list[3][1] + coords_list[4][1]) / 2))
+
+    closest_dist = min(left_ankle_to_bbox, right_ankle_to_bbox)
+
+    valeo_left_foot = (left_ankle[0], left_ankle[1] + closest_dist)
+    valeo_right_foot = (right_ankle[0], right_ankle[1] + closest_dist)
+    valeo_middle_point = (int((coords_list[11][0] + coords_list[12][0]) / 2),
+                          int((coords_list[11][1] + coords_list[12][1]) / 2))
+
+    # the following is used to extend ears point height to the top of the head
+    theta = calc_angle(ears_middle_point, valeo_middle_point)
+    torso_magnitude = math.sqrt((ears_middle_point[0]-valeo_middle_point[0])**2 +
+                                (ears_middle_point[1]-valeo_middle_point[1])**2)
+    p_value = 0.118
+    head_top = project_point_along_line(ears_middle_point, theta, torso_magnitude, p_value)
+
+    return dict(left_foot=valeo_left_foot, right_foot=valeo_right_foot,
+                middle_point=valeo_middle_point, top_point=head_top)
+
+
+def draw_valeo_keypoints(img, img_fname, valeo_ann, args):  ## debug
+    '''
+    :param img:
+    :param img_fname:
+    :param valeo_ann:
+    :param args:
+    :return:
+    '''
+    new_path = '{}/{}_valeo_annotation.jpg'.format(args.output_path, os.path.splitext(os.path.basename(img_fname))[0])
 
     if  os.path.exists(new_path):  # accumulate instances
         img = mmcv.imread(new_path)
 
-    # for idx, coords in enumerate(coords_list):
-    #     if idx in [1, 2, 3, 4]: continue
-    #
-    #     cv2.circle(img, coords, 4, (0, 255, 255), -1)
-    #     cv2.putText(img, str(idx)+': '+str(scores[0][idx][0])[:3], (coords[0]+5, coords[1]),
-    #                 cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.circle(img, valeo_ann[0], 4, (0, 0, 255), -1)
+    cv2.circle(img, valeo_ann[1], 4, (0, 0, 255), -1)
+    cv2.circle(img, valeo_ann[2], 4, (0, 0, 255), -1)
+    cv2.circle(img, valeo_ann[3], 4, (0, 0, 255), -1)
 
-    # print((final_bbox_coords[0][0], final_bbox_coords[1][1]), tuple(final_bbox_coords[1]))
+    cv2.line(img, valeo_ann[0], valeo_ann[1], (0, 0, 255), 2, 1)
+    cv2.line(img, valeo_ann[1], valeo_ann[2], (0, 0, 255), 2, 1)
+    cv2.line(img, valeo_ann[2], valeo_ann[3], (0, 0, 255), 2, 1)
 
-
-    bbox_rightbottom = final_bbox_coords[1]
-
-    left_ankle = coords_list[15]
-    left_ankle_to_bbox = abs(left_ankle[1]-bbox_rightbottom[1])
-    right_ankle = coords_list[16]
-    right_ankle_to_bbox = abs(right_ankle[1]-bbox_rightbottom[1])
-
-    # closest_foot = left_ankle if left_ankle_to_bbox < right_ankle_to_bbox else right_ankle
-    closest_dist = min(left_ankle_to_bbox, right_ankle_to_bbox)
-
-    valeo_left_foot = (left_ankle[0], left_ankle[1]+closest_dist)
-    valeo_right_foot = (right_ankle[0], right_ankle[1]+closest_dist)
-    valeo_middle_point = (int((coords_list[11][0]+coords_list[12][0])/2), int((coords_list[11][1]+coords_list[12][1])/2))
-    valeo_top_point = (int((coords_list[3][0]+coords_list[4][0])/2), int((coords_list[3][1]+coords_list[4][1])/2))
-
-    print(valeo_middle_point, valeo_top_point)
-
-    theta = calc_angle(valeo_middle_point, valeo_top_point)
-    new_top_point = project_point_along_line(valeo_top_point, theta, 10.0)
+    mmcv.imwrite(img, new_path)
 
 
-    cv2.line(img, valeo_left_foot, valeo_right_foot, (0, 0, 255), 2, 1)
-    cv2.line(img, valeo_right_foot, valeo_middle_point, (0, 0, 255), 2, 1)
-    cv2.line(img, valeo_middle_point, new_top_point, (0, 0, 255), 2, 1)
+def draw_all_keypoints(img, img_fname, coords_list, scores, final_bbox_coords, args):  ## debug
+    '''
+    :param img:
+    :param img_fname:
+    :param coords_list:
+    :param scores:
+    :param final_bbox_coords:
+    :param args:
+    :return:
+    '''
+    new_path = '{}/{}_poses.jpg'.format(args.output_path, os.path.splitext(os.path.basename(img_fname))[0])
 
+    if  os.path.exists(new_path):  # accumulate instances
+        img = mmcv.imread(new_path)
 
+    for idx, coords in enumerate(coords_list):
+        if idx in [1, 2, 3, 4]: continue
+
+        cv2.circle(img, coords, 4, (0, 255, 255), -1)
+        cv2.putText(img, str(idx)+': '+str(scores[0][idx][0])[:3], (coords[0]+5, coords[1]),
+                    cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+
+    # BBOX
+    cv2.rectangle(img, tuple(final_bbox_coords[0]), tuple(final_bbox_coords[1]), (0, 0, 255), 2)
+
+    ## Basic skeleton
     # line_color = (255, 255, 0)
     # line_thickness = 2
     # cv2.line(img, coords_list[0], coords_list[5], line_color, line_thickness)
@@ -169,7 +224,6 @@ def draw_final_points(img, img_fname, coords_list, scores, final_bbox_coords, ou
     # cv2.line(img, coords_list[13], coords_list[15], line_color, line_thickness)
     # cv2.line(img, coords_list[14], coords_list[16], line_color, line_thickness)
 
-
     mmcv.imwrite(img, new_path)
 
     # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
@@ -178,14 +232,14 @@ def draw_final_points(img, img_fname, coords_list, scores, final_bbox_coords, ou
     # cv2.destroyAllWindows()
 
 
-def forward_and_parse(outputs, bboxes_data, input_path, output_path, save_images):
+def forward_and_parse(outputs, bboxes_data, args):
     '''
     :param outputs:
     :param bboxes_data:
     :param input_path:
     :return:
     '''
-    prev_fname = None
+
     result_dict = dict()
     for idx, output in enumerate(list(outputs)):
         if isinstance(output, tuple):
@@ -204,7 +258,7 @@ def forward_and_parse(outputs, bboxes_data, input_path, output_path, save_images
         bbox_shape = bbox_img.shape[:2]
 
         basefname = bboxes_data['img_fnames'][idx]
-        img_fname = '{}/{}'.format(input_path, basefname)
+        img_fname = '{}/{}'.format(args.input_path, basefname)
         full_img = mmcv.imread(img_fname)
 
         lefttop_point = bboxes_data['final_bbox_coords'][idx][0]
@@ -215,25 +269,33 @@ def forward_and_parse(outputs, bboxes_data, input_path, output_path, save_images
             full_img_coords = (bbox_coords[0] + lefttop_point[0], bbox_coords[1] + lefttop_point[1])
             coords_list.append(full_img_coords)
 
+        if args.valeo:
+            scores = None
+            valeo_ann = get_valeo_pd_ann(coords_list, final_bbox_coords[idx])
+            coords_list = list(valeo_ann.values())
+            sc = ['0.0' for s in range(len(coords_list))]
+        else:
+            sc = [str(s[0])[:5] for s in scores[0]]
+
         if basefname not in list(result_dict):
-            result_dict[basefname] = dict(keypoints=coords_list, scores=[str(s[0])[:5] for s in scores[0]])
+            result_dict[basefname] = dict(keypoints=coords_list, scores=sc)
         else:
             result_dict[basefname]['keypoints'] += coords_list
-            result_dict[basefname]['scores'] += [str(s[0])[:5] for s in scores[0]]
+            result_dict[basefname]['scores'] += sc
 
-        if save_images:
-            # print(final_bbox_coords[idx], idx)
-            draw_final_points(full_img, img_fname, coords_list, scores, final_bbox_coords[idx], output_path)  # debug
+        if args.save_images:
+            if args.valeo:
+                draw_valeo_keypoints(full_img, img_fname, coords_list, args)
+            else:
+                # print(final_bbox_coords[idx], idx)
+                draw_all_keypoints(full_img, img_fname, coords_list, scores, final_bbox_coords[idx], args)
 
     # Save JSON file
-    with open('{}_keypoints.json'.format(time.strftime("%Y%m%d%H%M%S")), 'w') as out_file:
+    with open('{}/{}_keypoints.json'.format(args.output_path, time.strftime("%Y%m%d%H%M%S")), 'w') as out_file:
         json.dump(result_dict, out_file)
 
 
 def main():
-    '''
-    Usage example:  python3 run_on_custom_patches.py -i ./input/images -js ./input/json/20190521073448_detection_bboxes.json -o ./pose_results
-    '''
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_path", help="Path to directory containing images OR path to a single image",
                         required=True)
@@ -241,19 +303,17 @@ def main():
                         required=True)
     parser.add_argument("-o", "--output_path", help="Desired output dir",
                         required=True)
-    parser.add_argument("-s", "--save_images", help="True/False",
-                        default=False, required=False)
+    parser.add_argument("-s", "--save_images", action='store_true')
+    parser.add_argument('-v', '--valeo', help='Valeo PD annotation', action='store_true')
     args = parser.parse_args()
 
-    # Params
+    args.input_path = remove_trailing_slash(args.input_path)
+    args.output_path = remove_trailing_slash(args.output_path)
+
+    # Static params
     config_file = '../experiments/coco/hrnet/w48_384x288_adam_lr1e-3.yaml'
     cfg_mmcv = mmcv.Config.fromfile('../lib/config/mmcv_config.py')
     checkpoint = '../lib/models/pytorch/pose_coco/pose_hrnet_w48_384x288.pth'
-
-    input_path = args.input_path
-    json_path = args.input_json
-    output_path = args.output_path
-    save_images = eval(args.save_images)
 
     # Trying to simplify bullshit cfg parsing without modifying get_pose_net() method
     cfg.cfg = config_file
@@ -261,7 +321,7 @@ def main():
     update_config(cfg, cfg)  # lol
     ##
 
-    bboxes_data = read_bboxes(json_path, input_path)
+    bboxes_data = read_bboxes(args.input_json, args.input_path)
     bboxes = bboxes_data['bboxes']
 
     model_input_width = cfg.MODEL.IMAGE_SIZE[1]
@@ -278,7 +338,7 @@ def main():
     else:
         raise Exception('There are no bboxes!')
 
-    forward_and_parse(outputs, bboxes_data, input_path, output_path, save_images)
+    forward_and_parse(outputs, bboxes_data, args)
 
 
 
